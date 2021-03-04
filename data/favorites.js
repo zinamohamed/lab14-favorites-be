9fc5963cc5648ba4d5d449a56ea5b1522969220e
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-03-03 21:41:42 -0800

    join a coding bootcamp they said

--- data/favorites.js
+++ data/favorites.js
@@ -1,11 +1,12 @@
 module.exports = [
   {
-    image_link: 'https://cdn.shopify.com/s/files/1/1338/0845/collections/lippie-pencil_grande.jpg?v=1512588769',
+    image_link: 'https://cdn.shopify.com/s/files/1/1338/0845/products/brain-freeze_a_800x1200.jpg?v=1502255076',
     brand:'colourpop',
-    name: 'Lippie Pencil',
-    product_type: 'lip_liner',
-    description: 'Lippie Pencil A long-wearing and high-intensity lip pencil that glides on easily and prevents feathering. Many of our Lippie Stix have a coordinating Lippie Pencil designed to compliment it perfectly, but feel free to mix and match!',
-    db_id: 1048
+    name: 'Blotted Lip',
+    product_type: 'lipstick',
+    description: 'Blotted Lip Sheer matte lipstick that creates the perfect popsicle pout! Formula is lightweight, matte and buildable for light to medium coverage.',
+    db_id: 1047,
+    
   },
   {
     image_link: 'https://www.nyxcosmetics.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-cpd-nyxusa-master-catalog/default/dw663a4cdb/ProductImages/Lips/Soft_Matte_Lip_Cream/softmattelipcream_main.jpg?sw=390\u0026sh=390\u0026sm=fit',
@@ -13,6 +14,7 @@ module.exports = [
     name: 'Soft Matte Lip Cream',
     product_type: 'lipstick',
     description: 'Lippie Pencil A long-wearing and high-intensity lip pencil that glides on easily and prevents feathering. Many of our Lippie Stix have a coordinating Lippie Pencil designed to compliment it perfectly, but feel free to mix and match!',
-    db_id: 852
+    db_id: 852,
+    
   }
 ];
